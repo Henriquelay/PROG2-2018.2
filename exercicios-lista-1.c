3725525bb34e853ab1d1bc411562feddc34c0b6d
Henriquelay <henriquelayber@gmail.com>
2018-08-10 23:50:16 -0300

    Teste

---
+++ exercicios-lista-1.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+int main ()
+{
+        int leituraAtual = 125;
+        int leituraAnterior = 25;
+        float valorUnitario = 2.5;
+        int diferenca;
+        float valorConta;
+
+        diferenca = leituraAtual - leituraAnterior;
+        valorConta = diferenca * valorUnitario;
+
+        return 0;
+}
