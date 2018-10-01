9adaa0e38f1033727d7748e369f54292f78b9f7a
HenriqueLay <henriquelayber@gmail.com>
2018-10-01 17:39:02 -0300

    Slides

---
+++ lista4/lista4ex3.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void main(){
+        char c, S[20];
+
+
+        printf(" a. Ler uma string S1 (tamanho máximo 20 caracteres);\n b. Imprimir o tamanho da string S1;\n c. Comparar a string S1 com uma nova string S2 fornecida pelo usuário e imprimir o resultado da comparação;\n d. Concatenar a string S1 com uma nova string S2 e imprimir na tela o resultado da concatenação;\n e. Imprimir a string S1 de forma reversa;\n f. Contar quantas vezes um dado caractere aparece na string S1. Esse caractere desse ser informado pelo usuário;\n g. Retornar uma substring da string S1. Para isso o usuário deve informar a partir de qual posição deve ser criada a substring e qual é o tamanho da substring.\n h. Substituir a primeira ocorrência do caractere C1 da string S1 pelo caractere C2. Os caracteres C1 e C2 serão lidos pelo usuário;\n0. Terminar o programa\n\n>>>");
+
+        scanf(" %c", &c);
+
+        switch(c);
+                case a:
+                sscanf(" %s", &S1);
+}
