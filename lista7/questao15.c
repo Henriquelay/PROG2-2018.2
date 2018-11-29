3662ec60d49bef58bce485d57253fb12585a1d87
HenriqueLay <henriquelayber@gmail.com>
2018-11-29 17:18:48 -0200

    update (q15 imcompleta)

---
+++ lista7/questao15.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int N;
+void inverte_vetor(int V1, int V2){
+    for(int i = N; i > 0 ; i--)
+        V2[N-i] = V1[N];
+}
+
+main(int argc, char *argv){
+    s    
+}
--- lista9/questao1.c
+++ lista9/questao1.c
@@ -1,20 +1,85 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int existeEm(int x1, int* x2, int n2){
-        for(int x = 0; x < n2; x++)
-            if(x1 == x2[x]) return 1;
-
+int ehMaior(int *a, int *c){
+    if(*a > *c) return 1;
     return 0;
 }
 
-int* uniao(int* x1, int* x2, int n1, int n2, int* qtd){
-    *qtd = n1 + n2;
+void fazSwap(int **vet1r, int **vet2r){
+    int *swap, *vet1 = *vet1r, *vet2 = *vet2r;
+
+    swap = vet1;
+    vet1 = vet2;
+    vet2 = swap;
+    *vet1r = vet1;
+    *vet2r = vet2;    
+}
+
+void chegaPraLa(int **vet2r, int *size2, int *indicePartida){
+    int *vet2 = *vet2r;
+    for(int i = *indicePartida; i < *size2 - 1; i++)
+        vet2[i] = vet2[i+1];
     
-    for(int i = 0; i < n1; i++){
-        if(existeEm(x1[i], x2, n2)) *qtd--;
-        else{
-            *qtd++;
-        }
+    *size2--;
+    vet2 = (int*) realloc(vet2, sizeof(int) * *size2);
+}
+
+void removerOcorDup(int **vet1r, int *size1r, int **vet2r, int *size2r){
+    int *vet1 = *vet1r, *vet2 = *vet2r;
+    int tamanhofinal = *size2r;
+
+    for(int i = 0; i < *size1r; i++)
+        for(int j = 0; j < *size2r; j++)
+            if(vet1[i] == vet2[j])
+                chegaPraLa(&vet2, size2r, &j);
+}
+
+//supondo que cada vetor individualmente não tem repetição
+int* uniao(int **vet1r, int **vet2r, int *size1, int *size2){
+    int *vet1 = *vet1r, *vet2 = *vet2r;
+    if(ehMaior(size2, size1)){
+        fazSwap(&vet1, &vet2);
     }
+    removerOcorDup(&vet1, size1, &vet2, size2);
+    //sizes será atualizado apropriadamente
+    int *resultado = (int*) malloc(sizeof(int) * (*size1 + *size2));
+
+    int i = 0;
+    for(i; i < *size1; i++)
+        resultado[i] = vet1[i];
+    for(i; i< *size2; i++)
+        resultado[i] = vet2[i];
+
+    return resultado;
+}
+
+
+int main(){
+    int *vet1, *vet2, size1, size2;
+
+    puts(">Tamanho do vet 1 e vet 2: ");
+    scanf("%i %i", &size1, &size2);
+
+    vet1 = (int*) malloc(sizeof(int) * size1);
+    vet2 = (int*) malloc(sizeof(int) * size2);
+
+    for(int i = 0; i < size1; i++){
+        printf(">VET1[%i] = ", i);
+        scanf("%i", &vet1[i]);
+    }
+    for(int i = 0; i < size1; i++){
+        printf(">VET2[%i] = ", i);
+        scanf("%i", &vet1[i]);
+    }
+    
+    int *resposta = uniao(&vet1, &vet2, &size1, &size2);
+    
+    puts("==RESULTADO:==");
+    for(int i = 0; i < (size1 + size2); i++)
+        printf("VET[%i] = %i\n", i, resposta[i]);
+
+
+    free(vet1);
+    free(vet2);
 }
