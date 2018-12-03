1958626920c2c2defba310c7b4e90bb60f11c395
henriquelay <henriquelayber@gmail.com>
2018-12-03 21:17:46 -0200

    aaaaaa

--- lista9/questao1.c
+++ lista9/questao1.c
@@ -1,38 +1,46 @@
-#include <stdlib.h>
+]#include <stdlib.h>
 #include <stdio.h>
 
 int* concatInt(int *x1, int *x2, int n1, int n2, int *qtd){
     int* result = (int*) malloc(sizeof(int) * (n1 + n2));
-
+    printf("TAM \n===== %i\n", *qtd);
     int i = 0;
     for(i ; i < n1; i++)
         result[i] = x1[i];
-    *qtd = n1+n2;
     for(i; i < *qtd; i++)
         result[i] = x2[i - n1];
 
     return result;
 }
 
-void chegaPraLa(int *vet, int *tam, int i, int j){
+int* chegaPraLa(int *vet, int *tam, int j){
+    *tam--;
+    for(int i = j; i < *tam; i++)
+        vet[i] = vet[i+1];
+    
+    printf("TAM O PORRA\n===== %i\n", *tam);
+    int *result = (int*) malloc(sizeof(int) * *tam);
+    for(int i = 0; i < *tam; i++)
+        result[i] = vet[i];
+    free(vet);
 
-    //continuar
+    return result;
 }
 
-void removeRepet(int *vet, *tam){
-    for(int i = 0; i < *tam; i++){
+void removeRepet(int *vet, int *tam){
+    printf("TAM \n===== %i\n", *tam);
+    for(int i = 0; i < *tam; i++)
         for(int j = 0; j < *tam; j++){
             if(i == j) continue;
-            if(vet[i] == vet[j]) chegaPraLa(vet, tam, i, j);
+            if(vet[i] == vet[j]) vet = chegaPraLa(vet, tam, j);
         }
-    }
 }
 
-
 int* uniao(int *x1, int *x2, int n1, int n2, int *qtd){
+    printf("TAM \n===== %i\n", *qtd);
     int* result = concatInt(x1, x2, n1, n2, qtd);
-
-    void removeRepet(result,qtd);
+    printf("TAM \n===== %i\n", *qtd);
+    removeRepet(result,qtd);
 
     return result;
 }
@@ -42,23 +50,34 @@ int* uniao(int *x1, int *x2, int n1, int n2, int *qtd){
 int main(){
     int tamVet[2];
 
-
     puts("Tamanho vetor 1 e 2: ");
-    scanf("%i %i", &tamVet[0], &tamVet[1]);
+    scanf("%i %i", &(tamVet[0]), &(tamVet[1]));
 
     int *vet1 = (int*) malloc(sizeof(int) * tamVet[0]);
     int *vet2 = (int*) malloc(sizeof(int) * tamVet[1]);
 
-    int tamResult;
-    int *vetResult = uniao(vet1, vet2, tamVet[0], tamVet[1], &tamResult);
+    for(int i = 0; i < tamVet[0]; i++){
+        printf("Vet1[%i]: ", i + 1);
+        scanf("%i", &vet1[i]);
+    }
+    for(int i = 0; i < tamVet[1]; i++){
+        printf("Vet2[%i]: ", i + 1);
+        scanf("%i", &vet2[i]);
+    }
+
+    int* tamResult = (int*) malloc(sizeof(int));
+    *tamResult = tamVet[0] + tamVet[1];
+    printf("TAM conteudo \n===== %i\n", tamResult);
+    int *vetResult = uniao(vet1, vet2, tamVet[0], tamVet[1], tamResult);
 
     free(vet1);
     free(vet2);
 
-    puts("Resultado da uniao:\n");
-    for(int i = 0; i < tamResult; i++)
+    puts("Resultado da uniao:");
+    for(int i = 0; i < *tamResult; i++)
         printf("%i ", vetResult[i]);
 
+    free(tamResult);
     free(vetResult);
     return 0;
 }
